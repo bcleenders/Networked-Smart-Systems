6b6b195451b4c09a33ad143960a0bc7df5ceeaa8
Patrick van Looy <pvlooy@gmail.com>
2014-05-14 14:41:36 +0200

    kleine fix

--- week4/pseudocode.cpp
+++ week4/pseudocode.cpp
@@ -1,22 +1,23 @@
 const timespan = 100;
 const stepsize = 10;
+int counter = 0;
 
 // Slaap altijd iig de helft van de tijd
 sleep(timespan);
 
-int counter = 0;
 while(counter < timespan) {
     counter += stepsize;
 
     if(counter >= timespan) {
-        // Stuur een signaal
+        // Stuur een signaal & laat LED knipperen
         radio.send(); // TODO; echte functie gebruiken!
+        digitalWrite(led_pin, HIGH); // laat LED branden
 
         // Reset de state van de machine
         counter = 0;
     }
 
-    if(radio.available()) {
+    if(radio.available()) {// ontvang signaal
         counter = timespan - (timespan-counter)/2
     }
 
