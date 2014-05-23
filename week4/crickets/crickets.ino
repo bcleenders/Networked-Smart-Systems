f7c579f41ea78a7d1e51d0a8af866247ac2becc2
Bram Leenders <bcleenders@gmail.com>
2014-05-23 14:49:42 +0200

    Aan verslag gewerkt

--- week4/crickets/crickets.ino
+++ week4/crickets/crickets.ino
@@ -2,7 +2,7 @@
 #include "printf.h"
 
 const int led_pin = 7;
-const int threshold = 40;
+const float thresholdfactor = 3.0;
 
 void setup(void)
 {
@@ -19,34 +19,43 @@ int counter = 0;
 
 int measure1 = 0;
 int measure2 = 0;
+float avgdiff = 0.0;
 
 void loop(void) {
     if(counter >= timespan) {
         printf("counter >= timespan\n");
 
 // Stuur een signaal (poort 8, 1334 Hz, 100ms)
-        tone(8, 1334, 30);
-        delay(timespan - 30);
+        tone(8, 3000, stepsize);
+        delay(timespan - stepsize);
 
 // Reset de state van de machine
         counter = 0;
     }
 
-    if(counter % 4 == 0) {
-      printf("m1 %3d  --- m2 %3d\n", measure1, measure2);
-    }
+    float diff = abs(measure1-measure2);
+    float difffloat = (float) diff;
+    avgdiff = ((10.0 * difffloat) + 90.0*avgdiff)/100.0; // Slowly adjust avgdiff
 
-    if(measure1 - measure2 > threshold || measure2 - measure1 > threshold) { // ontvang signaal
-        printf("Received signal - m1 %3d  --- m2 %3d\n", measure1, measure2);
+    if(difffloat > (avgdiff*thresholdfactor)) { // ontvang signaal
+
+        Serial.print("Received signal, m1=");
+        Serial.print(measure1);
+        Serial.print(", m2=");
+        Serial.print(measure2);
+        Serial.print(", diff=");
+        Serial.print(diff);
+        Serial.print(", avgdiff=");
+        Serial.print(avgdiff);
+        Serial.print("\n");
+      
         counter = timespan - (timespan-counter)/3;
         measure1 = 0;
         measure2 = 0;
     }
-    else {
-       measure1 = analogRead(A0);
-       delay(stepsize);
-       counter += stepsize;
-       measure2 = analogRead(A0);
-    }
+     measure1 = analogRead(A0);
+     delay(stepsize);
+     counter += stepsize;
+     measure2 = analogRead(A0);
 }
 
