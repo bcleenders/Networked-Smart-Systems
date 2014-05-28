da2ed190841664e0bf5e60ac4a41a18936e6c4b6
Bram Leenders <bcleenders@gmail.com>
2014-05-28 15:33:02 +0200

    Started testing for week5

---
+++ week5/microphone_test_ino/microphone_test_ino.ino
@@ -0,0 +1,118 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+RF24 radio(3, 9);
+const int role_pin = 7;
+
+void setup() {
+  // We willen radio luisteren
+  pinMode(role_pin, INPUT);
+  delay(20);
+  
+    // initialize the serial communication:
+  Serial.begin(9600);
+  
+  // Setup and configure rf radio
+    radio.begin();
+    radio.setRetries(0,0);
+
+    radio.setDataRate(RF24_2MBPS);
+    radio.setChannel(76);
+    radio.setPayloadSize(1);
+    radio.openReadingPipe(1, 0xdeadbeefa1LL);
+    radio.startListening();
+    radio.printDetails();
+}
+
+unsigned long radiotime;
+unsigned long audiotime;
+int prevMeasure = 0;
+int currMeasure = 0;
+
+void loop() {
+  
+  while (! radio.available()) {}
+  radiotime = micros();
+  uint8_t activeBeacon;
+  radio.read( &activeBeacon, sizeof(uint8_t));
+  Serial.print(activeBeacon);
+            
+  do {
+    prevMeasure = currMeasure;
+    currMeasure = analogRead(A0);
+  }
+  while(currMeasure - prevMeasure < 100);
+  
+  audiotime = micros();
+  
+  Serial.print(" ");
+  Serial.println(audiotime - radiotime);
+  
+  // wait a bit for the analog-to-digital converter 
+  // to stabilize after the last reading:
+  delay(10);
+}
+
+/* Processing code:
+
+// Graphing sketch
+ 
+ 
+ // This program takes ASCII-encoded strings
+ // from the serial port at 9600 baud and graphs them. It expects values in the
+ // range 0 to 1023, followed by a newline, or newline and carriage return
+ 
+ import processing.serial.*;
+ 
+ Serial myPort;        // The serial port
+ int xPos = 1;         // horizontal position of the graph
+ 
+ void setup () {
+ // set the window size:
+ size(400, 300);        
+ 
+ // List all the available serial ports
+ println(Serial.list());
+ // I know that the first port in the serial list on my mac
+ // is always my  Arduino, so I open Serial.list()[0].
+ // Open whatever port is the one you're using.
+ myPort = new Serial(this, Serial.list()[0], 9600);
+ // don't generate a serialEvent() unless you get a newline character:
+ myPort.bufferUntil('\n');
+ // set inital background:
+ background(0);
+ }
+ void draw () {
+ // everything happens in the serialEvent()
+ }
+ 
+ void serialEvent (Serial myPort) {
+ // get the ASCII string:
+ String inString = myPort.readStringUntil('\n');
+ 
+ if (inString != null) {
+ // trim off any whitespace:
+ inString = trim(inString);
+ // convert to an int and map to the screen height:
+ float inByte = float(inString); 
+ inByte = map(inByte, 0, 1023, 0, height);
+ 
+ // draw the line:
+ stroke(127,34,255);
+ line(xPos, height, xPos, height - inByte);
+ 
+ // at the edge of the screen, go back to the beginning:
+ if (xPos >= width) {
+ xPos = 0;
+ background(0); 
+ } 
+ else {
+ // increment the horizontal position:
+ xPos++;
+ }
+ }
+ }
+ 
+ */
