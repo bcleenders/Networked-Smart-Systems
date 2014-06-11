d9a4d63a1f85b64f2ff1545de1a3d7e910c3b6c9
Bram Leenders <bcleenders@gmail.com>
2014-06-11 14:39:10 +0200

    Taking average values of measurements (attempt #1)

--- week5/gps/gps.ino
+++ week5/gps/gps.ino
@@ -1,7 +1,7 @@
 /*
     GPS algorithm borrowed from: 
-        http://link.springer.com/chapter/10.1007/978-3-642-25486-4_59#page-1
-*/
+ http://link.springer.com/chapter/10.1007/978-3-642-25486-4_59#page-1
+ */
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -17,26 +17,93 @@ unsigned long timelimit = 50000LL;
 uint8_t activeBeacon;
 
 float pos[4][2] = { // Positions van de beacons; pos[1][1] is de y positie van beacon 1
-    {0.0, 30.0},
-    {80.0, 0.0},
-    {320.0, 0.0},
-    {400.0, 30.0}
+  {
+    0.0, 30.0        }
+  ,
+  {
+    80.0, 0.0        }
+  ,
+  {
+    320.0, 0.0        }
+  ,
+  {
+    400.0, 30.0        }
 };
 
-float A[N][N] = { // Relatieve afstanden tussen de nodes; gebruikt node 3 nog niet!
-    {2*pos[1][0] - 2*pos[0][0], 2*pos[1][1] - 2*pos[0][1], 0.000001},
-    {2*pos[2][0] - 2*pos[1][0], 2*pos[2][1] - 2*pos[1][1], 0.000001},
-    {2*pos[0][0] - 2*pos[2][0], 2*pos[0][1] - 2*pos[2][1], 0.000001}
+float A[4][N][N] = {
+  { // Relatieve afstanden tussen de nodes; gebruikt node 0 nog niet!
+    {
+      2*pos[2][0] - 2*pos[1][0], 2*pos[2][1] - 2*pos[1][1], 0.000001                }
+    ,
+    {
+      2*pos[3][0] - 2*pos[2][0], 2*pos[3][1] - 2*pos[2][1], 0.000001                }
+    ,
+    {
+      2*pos[1][0] - 2*pos[3][0], 2*pos[1][1] - 2*pos[3][1], 0.000001                }
+  }
+  ,
+  { // Relatieve afstanden tussen de nodes; gebruikt node 1 nog niet!
+    {
+      2*pos[2][0] - 2*pos[0][0], 2*pos[2][1] - 2*pos[0][1], 0.000001                }
+    ,
+    {
+      2*pos[3][0] - 2*pos[2][0], 2*pos[3][1] - 2*pos[2][1], 0.000001                }
+    ,
+    {
+      2*pos[0][0] - 2*pos[3][0], 2*pos[0][1] - 2*pos[3][1], 0.000001                }
+  }
+  ,
+  { // Relatieve afstanden tussen de nodes; gebruikt node 2 nog niet!
+    {
+      2*pos[1][0] - 2*pos[0][0], 2*pos[1][1] - 2*pos[0][1], 0.000001                }
+    ,
+    {
+      2*pos[3][0] - 2*pos[1][0], 2*pos[3][1] - 2*pos[1][1], 0.000001                }
+    ,
+    {
+      2*pos[0][0] - 2*pos[3][0], 2*pos[0][1] - 2*pos[3][1], 0.000001                }
+  }
+  ,
+  { // Relatieve afstanden tussen de nodes; gebruikt node 3 nog niet!
+    {
+      2*pos[1][0] - 2*pos[0][0], 2*pos[1][1] - 2*pos[0][1], 0.000001                }
+    ,
+    {
+      2*pos[2][0] - 2*pos[1][0], 2*pos[2][1] - 2*pos[1][1], 0.000001                }
+    ,
+    {
+      2*pos[0][0] - 2*pos[2][0], 2*pos[0][1] - 2*pos[2][1], 0.000001                }
+  }
 };
 
 // Static; just add d_0 and d_1
-float B_static[N] = {
-    /*d_0^2 - d_1^2 */ - (pos[0][0]*pos[0][0]) + (pos[1][0]*pos[1][0]) - (pos[0][1]*pos[0][1]) + (pos[1][1]*pos[1][1]) /* dz = 0 */,
-    /*d_1^2 - d_2^2 */ - (pos[1][0]*pos[1][0]) + (pos[2][0]*pos[2][0]) - (pos[1][1]*pos[1][1]) + (pos[2][1]*pos[2][1]) /* dz = 0 */,
-    /*d_2^2 - d_0^2 */ - (pos[2][0]*pos[2][0]) + (pos[0][0]*pos[0][0]) - (pos[2][1]*pos[2][1]) + (pos[0][1]*pos[0][1]) /* dz = 0 */
-};
+float B_static[4][N] = {
+  {
+    /*d_0^2 - d_1^2 */    - (pos[1][0]*pos[1][0]) + (pos[2][0]*pos[2][0]) - (pos[1][1]*pos[1][1]) + (pos[2][1]*pos[2][1]) /* dz = 0 */,
+    /*d_1^2 - d_2^2 */    - (pos[2][0]*pos[2][0]) + (pos[3][0]*pos[3][0]) - (pos[2][1]*pos[2][1]) + (pos[3][1]*pos[3][1]) /* dz = 0 */,
+    /*d_2^2 - d_0^2 */    - (pos[3][0]*pos[3][0]) + (pos[1][0]*pos[1][0]) - (pos[3][1]*pos[3][1]) + (pos[1][1]*pos[1][1]) /* dz = 0 */
+    }
+    ,
+  {
+    /*d_0^2 - d_1^2 */    - (pos[0][0]*pos[0][0]) + (pos[2][0]*pos[2][0]) - (pos[0][1]*pos[0][1]) + (pos[2][1]*pos[2][1]) /* dz = 0 */,
+    /*d_1^2 - d_2^2 */    - (pos[2][0]*pos[2][0]) + (pos[3][0]*pos[3][0]) - (pos[2][1]*pos[2][1]) + (pos[3][1]*pos[3][1]) /* dz = 0 */,
+    /*d_2^2 - d_0^2 */    - (pos[3][0]*pos[3][0]) + (pos[0][0]*pos[0][0]) - (pos[3][1]*pos[3][1]) + (pos[0][1]*pos[0][1]) /* dz = 0 */
+    }
+    ,
+  { // slaat 2 over
+    /*d_0^2 - d_1^2 */    - (pos[0][0]*pos[0][0]) + (pos[1][0]*pos[1][0]) - (pos[0][1]*pos[0][1]) + (pos[1][1]*pos[1][1]) /* dz = 0 */,
+    /*d_1^2 - d_2^2 */    - (pos[1][0]*pos[1][0]) + (pos[3][0]*pos[3][0]) - (pos[1][1]*pos[1][1]) + (pos[3][1]*pos[3][1]) /* dz = 0 */,
+    /*d_2^2 - d_0^2 */    - (pos[3][0]*pos[3][0]) + (pos[0][0]*pos[0][0]) - (pos[3][1]*pos[3][1]) + (pos[0][1]*pos[0][1]) /* dz = 0 */
+    }
+    ,
+  { // slaat 3 over
+    /*d_0^2 - d_1^2 */    - (pos[0][0]*pos[0][0]) + (pos[1][0]*pos[1][0]) - (pos[0][1]*pos[0][1]) + (pos[1][1]*pos[1][1]) /* dz = 0 */,
+    /*d_1^2 - d_2^2 */    - (pos[1][0]*pos[1][0]) + (pos[2][0]*pos[2][0]) - (pos[1][1]*pos[1][1]) + (pos[2][1]*pos[2][1]) /* dz = 0 */,
+    /*d_2^2 - d_0^2 */    - (pos[2][0]*pos[2][0]) + (pos[0][0]*pos[0][0]) - (pos[2][1]*pos[2][1]) + (pos[0][1]*pos[0][1]) /* dz = 0 */
+    }
+  };
 
-float D[4];
+  float D[4];
 
 
 void setup() {
@@ -57,9 +124,22 @@ void setup() {
   radio.setAutoAck(false);
   //radio.printDetails();
 
-  Matrix.Invert((float*)A,N);
-  Serial.println("\nInverted A:");
-  Matrix.Print((float*)A,N,N,"A");
+  Matrix.Invert((float*)A[0],N);
+  Matrix.Invert((float*)A[1],N);
+  Matrix.Invert((float*)A[2],N);
+  Matrix.Invert((float*)A[3],N);
+
+  Serial.println("\nInverted A[0]:");
+  Matrix.Print((float*)A[0],N,N,"A[0]");
+
+  Serial.println("\nInverted A[1]:");
+  Matrix.Print((float*)A[1],N,N,"A[1]");
+
+  Serial.println("\nInverted A[2]:");
+  Matrix.Print((float*)A[2],N,N,"A[2]");
+
+  Serial.println("\nInverted A[3]:");
+  Matrix.Print((float*)A[3],N,N,"A[3]");
 }
 
 void loop() {
@@ -71,7 +151,9 @@ void loop() {
   radiotime = micros();
   radio.read( &activeBeacon, sizeof(uint8_t));
 
-  if(activeBeacon > 3) { return; }
+  if(activeBeacon > 3) { 
+    return; 
+  }
 
   while(analogRead(A0) < 50) {
     audiotime = micros();
@@ -80,7 +162,7 @@ void loop() {
       return; 
     }
   }
-  
+
   float diff = audiotime - radiotime;
   diff = diff * 0.03432; // Afstand tot beacon in cm
 
@@ -92,14 +174,30 @@ void loop() {
 }
 
 void calcPosition() {
+  float P_avg[N];
+
+  for(int i = 0; i < 4; i++) {
     float B[N] = {
-        (D[0]*D[0]) - (D[1]*D[1]) + B_static[0],
-        (D[1]*D[1]) - (D[2]*D[2]) + B_static[1],
-        (D[2]*D[2]) - (D[0]*D[0]) + B_static[2]
+      (D[i      ]*D[i      ]) - (D[(i+1)%3]*D[(i+1)%3]) + B_static[i      ],
+      (D[(i+1)%3]*D[(i+1)%3]) - (D[(i+2)%3]*D[(i+2)%3]) + B_static[(i+1)%3],
+      (D[(i+2)%3]*D[(i+2)%3]) - (D[i      ]*D[i      ]) + B_static[(i+2)%3]
     };
 
     float P[N];
     Matrix.Multiply((float*)A,(float*)B,N,N,1,(float*)P);
-    Serial.println("\nPosition (P):");
-    Matrix.Print((float*)P,N,1,"P");
+
+    Matrix.((float*)P, (float*)P_avg, 1, N, (float*)P_avg);
+  }
+
+  P_avg[0] = P_avg[0]/4;
+  P_avg[1] = P_avg[1]/4;
+  P_avg[2] = P_avg[2]/4;
+  P_avg[3] = P_avg[3]/4;
+
+  Serial.println("\nPosition (P_avg):");
+  Matrix.Print((float*)P_avg,N,1,"P_avg");
 }
+
+
+
+
